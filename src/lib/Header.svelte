8e121d361ad817cf44c1b1bd8b5ae74674963fa9
Asail <asail2005aa.com>
2024-06-28 01:18:09 +0300

    header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import dayjs from 'dayjs';
+	import 'dayjs/locale/ar';
+
+	dayjs.locale('ar');
+
+	let period = dayjs().format('a') == 'م' ? 'مساء' : "صباح";
+</script>
+
+<header class="flex justify-between">
+    <div>
+       <h1 class="text-4xl mb-2r ">{period}الخير 👋🏼 </h1>
+       <h2 class="text-2xl text-surface-900/50">
+    {dayjs().format('اليوم , dddd D MMM YYYY')}
+    </h2>
+    </div>
+    <div class="bg-red-400 w-32 h-10"> </div>
+</header>
