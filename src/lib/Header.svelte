6d8c657728d627e78a02c616b7ad4b42b06fe7de
Asail <asail2005aa.com>
2024-07-12 02:17:00 +0300

    locals

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,18 +1,64 @@
 <script lang="ts">
 	import dayjs from 'dayjs';
 	import 'dayjs/locale/ar';
+    import{
+        ListBox,
+        ListBoxItem,
+        popup,
+        type PopupSettings,
+    }from "@skeletonlabs/skeleton";
 
 	dayjs.locale('ar');
 
-	let period = dayjs().format('a') == 'م' ? 'مساء' : "صباح";
+	let period = dayjs().format('a') == 'م' ? 'مساء' : " صباح" ;
+
+
+   
+let comboboxValue: string ='مهام اليوم';
+
+const popupCombobox: PopupSettings = {
+	event: 'click',
+	target: 'popupCombobox',
+	placement: 'bottom',
+	closeQuery: '.listbox-item'
+};
+				
+		
 </script>
 
 <header class="flex justify-between">
     <div>
-       <h1 class="text-4xl mb-2r ">{period}الخير 👋🏼 </h1>
-       <h2 class="text-2xl text-surface-900/50">
+       <h1 class="text-4xl mb-2r ">{period} الخير👋🏼  </h1>
+       <h2 class="text-2xl text-slate-400">
     {dayjs().format('اليوم , dddd D MMM YYYY')}
     </h2>
     </div>
-    <div class="bg-red-400 w-32 h-10"> </div>
+
+    
+<button 
+
+    class="btn variant-soft-surface text-2xl w-48 h-fit py-4 justify-between"
+	 use:popup={popupCombobox}>
+	<span >{comboboxValue }</span>
+	<span class="!m-0">
+		<svg class="w-6" xmlns="http://www.w3.org/2000/svg"
+		 viewBox="0 0 
+		 24 24"><path fill="currentColor" fill-rule
+		 ="evenodd" d="m6 7l6 6l6-6l2 2l-8 8l-8-8z
+		 "/></svg>
+	</span>
+
+</button>
+				
+
+<div class="card w-48 shadow-xl py-2 " data-popup="popupCombobox">
+	<ListBox rounded="rounded-none" active="bg-indigo-300/15">
+		<ListBoxItem bind:group={comboboxValue} name="medium" value="جميع المهام">جميع المهام</ListBoxItem>
+		<ListBoxItem bind:group={comboboxValue} name="medium" value="مهام اليوم">مهام اليوم</ListBoxItem>
+	</ListBox>
+	<div class="arrow bg-surface-100-800-token" />
+</div>
+					
+	
+
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
 	import Header from "$lib/Header.svelte";
+    import {name} from "$lib/stores/name";
+ 
+   name.update((name: string) => {
+	return name + "1";
+   })
+
+console.log($name);
+
+
 </script>
 
-<div class="p-16">
+<div class="p-16 flex flex-col gap-20">
 <Header/>
+<div class="input-group input-group-divider flex justify-between ">
+	<input class="flex-1 " type="text" placeholder="أدخل المهمة"  />
+	<button class="variant-soft-surface">إضافة</button>
+</div>
 </div>
